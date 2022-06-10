a54bdb9d758bf49142a70e66956ec70eca3d7ed8
Wilhelm Payne <wbpayne@wilhelmpayne.com>
2022-06-10 09:13:16 -0400

    Cleanup

--- sysname.c
+++ sysname.c
@@ -7,7 +7,7 @@ int main(int ac, char *av[]) {
 	struct utsname a;
 	uname(&a);
 	pid_t p = getpid();
-	printf("%s[%d]: You are running %s ver %s on a(n) %s.\n", basename(av[0]), getpid(), a.sysname, a.release, a.machine);
+	printf("%s[%d]: You are running %s ver %s on a(n) %s.\n", basename(av[0]), p, a.sysname, a.release, a.machine);
 	return 0;
 }
 
