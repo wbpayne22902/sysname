d5c27a391f09a5ccc083b9560978330ff37313c7
wbpayne22902 <107161609+wbpayne22902@users.noreply.github.com>
2022-06-11 07:59:37 -0400

    changed whitespace

--- sysname.c
+++ sysname.c
@@ -9,11 +9,11 @@
 #include <unistd.h>
 
 int main(int ac, char *av[]) {
-	struct utsname a;
-	uname(&a);
-	pid_t p = getpid();
-	printf("%s[%d]: You are running %s ver %s on a(n) %s.\n", basename(av[0]), p, a.sysname, a.release, a.machine);
-	printf("Now exiting...\n");
-	return 0;
+    struct utsname a;
+    uname(&a);
+    pid_t p = getpid();
+    printf("%s[%d]: You are running %s ver %s on a(n) %s.\n", basename(av[0]), p, a.sysname, a.release, a.machine);
+    printf("Now exiting...\n");
+    return 0;
 }
 
