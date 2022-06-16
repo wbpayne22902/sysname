032b62b816ce8a26160ab1901714da40a5918053
Wilhelm Payne <wbpayne@wilhelmpayne.com>
2022-06-15 21:40:55 -0400

    removed poo emoji to be more professional

--- sysname.c
+++ sysname.c
@@ -13,7 +13,7 @@ int main(int ac, char *av[]) {
 	struct utsname a;
 	uname(&a);
 	pid_t p = getpid();
-	printf("%s[%d]: You are running %s ver %s on a(n) %s.\n 💩", basename(av[0]), p, a.sysname, a.release, a.machine);
+	printf("%s[%d]: You are running %s ver %s on a(n) %s.\n", basename(av[0]), p, a.sysname, a.release, a.machine);
 	printf("Now exiting...\n");
 	return 0;
 }
