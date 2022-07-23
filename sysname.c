7602f78a084dbc03249f0b29fb38c3240f7eb995
Wilhelm Payne <wbpayne@wilhelmpayne.com>
2022-07-23 12:21:21 -0400

    Fixed bug blocking compile
    Changed identifier in printf call.

--- sysname.c
+++ sysname.c
@@ -15,7 +15,7 @@ int main(int ac, char *av[]) {
 	uname(&a);
 	pid_t p = getpid();
 	pid_t pp = getppid();
-	printf("%s[%d:%d]: You are running %s ver %s on a(n) %s.\n", basename(av[0]), p, ppid, a.sysname, a.release, a.machine);
+	printf("%s[%d:%d]: You are running %s ver %s on a(n) %s.\n", basename(av[0]), p, pp, a.sysname, a.release, a.machine);
 	for (int i = 1; i <= 15; i++) {
 		printf("i[%d] = %d.\t", i, i);
 	}
