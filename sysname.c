914ee72c1612f52ad27cf1345cac54f3c5390492
Wilhelm Payne <wbpayne@wilhelmpayne.com>
2022-08-26 02:34:35 -0400

    Added cool
    Cool added

--- sysname.c
+++ sysname.c
@@ -1,7 +1,7 @@
 /*
    Welcome to my app.
    This app is a simple console app that tells you what Unix-based system
-   you are running.
+   you are running. Cool
 */
 #include <stdio.h>
 #include <stdlib.h>
