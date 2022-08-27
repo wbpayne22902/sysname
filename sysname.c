f95d64bb30d56602a8a74d6ea5dca91d821a5ae6
Wilhelm Payne <wbpayne@wilhelmpayne.com>
2022-08-27 00:13:25 -0400

    Added file writing capability
    Added filewriting capability using dprintf() and open()

--- sysname.c
+++ sysname.c
@@ -9,7 +9,9 @@
 #include <sys/types.h>
 #include <libgen.h>
 #include <unistd.h>
+#include <fcntl.h>
 
+#define mainname "sysout.txt"
 int main(int ac, char *av[]) {
 	struct utsname a;
 	uname(&a);
@@ -19,7 +21,9 @@ int main(int ac, char *av[]) {
 	for (int i = 1; i <= 15; i++) {
 		printf("i[%d] = %d.\t", i, i);
 	}
-	
+	int fd = open(mainname, O_CREAT | O_WRONLY | O_APPEND, 0644);
+	dprintf(fd, "%s[%d:%d]: You are running %s ver %s on a(n) %s.\n", basename(av[0]), p, pp, a.sysname, a.release, a.machine);
+	close(fd);
 	printf("\n");
 	printf("Thank you for using %s.\n", basename(av[0]));
 	printf("Now exiting...\n");
