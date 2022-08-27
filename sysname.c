30352b90ca11beca3a54c77981f31a931a76f63d
Wilhelm Payne <wbpayne@wilhelmpayne.com>
2022-08-27 15:04:32 -0400

    Updated comment
    Updated comment to include file closing statement.

--- sysname.c
+++ sysname.c
@@ -2,7 +2,7 @@
    Welcome to my app.
    This app is a simple console app that tells you what Unix-based system
    you are running. Cool
-   It also writes the system name to a file.
+   It also writes the system name to a file and a notice that the file is being closed..
 */
 #include <stdio.h>
 #include <stdlib.h>
