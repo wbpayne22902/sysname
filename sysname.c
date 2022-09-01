834cab2a5703141ab096f77210de2aef618e4ca1
Wilhelm Payne <wbpayne@wilhelmpayne.com>
2022-09-01 14:45:42 -0400

    Added version number
    Added define for version number and app will now report it to stdout.

--- sysname.c
+++ sysname.c
@@ -11,14 +11,14 @@
 #include <libgen.h>
 #include <unistd.h>
 #include <fcntl.h>
-
+#define prodVer 0.7
 #define mainname "sysout.txt"
 int main(int ac, char *av[]) {
 	struct utsname a;
 	uname(&a);
 	pid_t p = getpid();
 	pid_t pp = getppid();
-	printf("%s[%d:%d]: You are running %s ver %s on a(n) %s.\n", basename(av[0]), p, pp, a.sysname, a.release, a.machine);
+	printf("%s[%d:%d] (version %g): You are running %s ver %s on a(n) %s.\n", basename(av[0]), p, pp, prodVer, a.sysname, a.release, a.machine);
 	for (int i = 1; i <= 15; i++) {
 		printf("i[%d] = %d.\t", i, i);
 	}
